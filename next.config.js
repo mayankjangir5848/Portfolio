61e65d79b0920f75dab71a2431a855a4c4d916d0
mayankjangir5848 <jangirmayank621@gmail.com>
2026-01-09 09:45:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true
-}
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.js'
+})
 
-module.exports = nextConfig
+module.exports = withNextra()
